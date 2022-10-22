6c9a6fd400700da60530ceeff0d9bf8c3cc141c8
GHKEN <tetsuya@ghken.com>
2022-10-23 00:24:41 +0900

    保育園の情報を表示するカードのスタイルを修正

--- components/NurserySchoolCard.tsx
+++ components/NurserySchoolCard.tsx
@@ -3,7 +3,7 @@ import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
-import { CardActionArea } from '@mui/material'
+import { Box, CardActionArea } from '@mui/material'
 
 export const NurserySchoolCard: React.FC<{
     url: string
@@ -14,22 +14,22 @@ export const NurserySchoolCard: React.FC<{
 }> = (props) => {
     const { url, image, schoolName, introduction, price } = props
     return (
-        <Card component="a" href={url}>
+        <Card component="a" href={url} sx={{ textDecoration: 'none' }}>
             <CardActionArea>
                 <CardMedia
                     component="img"
                     height="200"
                     image={image}
-                    alt="izuru"
+                    alt="保育園"
                 />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                         {schoolName}
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                        {introduction}
-                        <br />
-                        <br />
+                        <Box mb={2}>
+                            {introduction}
+                        </Box>
                         月額保育料: {price}（1歳児の場合）
                     </Typography>
                 </CardContent>
